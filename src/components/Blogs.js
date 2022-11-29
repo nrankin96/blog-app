09c38349ab87901b6a6a44f79fc3c7bac2ecb09b
nrankin96 <nykiarankin96@gmail.com>
2022-11-28 22:46:20 -0500

    creating card

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,31 +1,63 @@
 import { useEffect, useState } from "react";
+import Card from "./Card";
 
 function Blogs() {
 
-    const [blog, setBlog] = useState([]);
-
-    async function getBlogs() {
-       const response = await fetch(`https://newsapi.org/v2/everything?q=Apple&from=2022-11-26&sortBy=popularity&apiKey=ce7e5dddeca149598d2e6784e5745bac`);
-       const data = await response.json();
-        localStorage.setItem('blogs', JSON.stringify(data));
-        setBlog(data);
-    }
+    const [blogs, setBlog] = useState([]);
 
     useEffect(() => {
         getBlogs()
-    }, [blog]);
+    }, []);
 
-console.log(blog);
+
+    const getBlogs = async () => {
+
+        const check = localStorage.getItem('blogs');
+
+        if(check) {
+            setBlog(JSON.parse(check))
+        } else {
+            const response = await fetch(`https://newsapi.org/v2/everything?q=technology&from=2022-11-26&sortBy=popularity&pageSize=5&apiKey=${process.env.REACT_APP_API_KEY}`);
+            const data = await response.json();
+            localStorage.setItem('blogs', JSON.stringify(data.articles));
+            setBlog(data.articles);
+        }
+
+       
+    }
+
+    
+console.log(blogs);
 
 
   return (
-        <div className="grid-cols-2">
-            <div className="bg-slate-500 w-80">
-               xx
-            </div>
+        <div>
+            <h1 className="font-semibold text-2xl m-2">Featured Stories</h1>
+            {blogs.map((articles) =>{
+                return(
+                    <div key={articles.id} className='m-4 flex bg-sky-600'>
+                        <div className="m-5">
+                            <img src={ articles.urlToImage} className='object-cover h-48 w-96'></img>
+                        </div>
+                        <div className="m-4 p-2 pl-5">
+                            <a className="font-semibold" href={articles.url}>{articles.title}</a>
+                            <p>{articles.description}</p>
+                        </div>
+                    </div>
+                    
+                   
+
+                );
+            })}
         </div>
     
   )
 }
 
 export default Blogs;
+
+{/* <div key={articles.id} className='m-4 border border-sky-500 w-48' >
+                            <img src={ articles.urlToImage}></img>
+                            <h2 >{articles.title}</h2>
+                             <p>{articles.description}</p>
+                        </div> */}
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <img></img>
+    </div>
+  )
+}
+
+export default Card
